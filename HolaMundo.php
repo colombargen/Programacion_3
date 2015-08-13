5e2b28bd5202dcf525c96d2f0ade512fa6d766d4
colombargen <magli.cp@gmail.com>
2015-08-13 20:51:45 -0300

    Test1
    primer programa

---
+++ HolaMundo.php
@@ -0,0 +1,23 @@
+/*<html>
+	<head>
+	</head>
+	<body>
+		<h1>    Hola mundo!!</h1>
+	</body>
+</html>
+*/
+
+<br>
+<p></p>
+
+<?php
+	echo "Anda este codigo?";
+?>
+<p></p>
+<p></p>
+<?php
+	$cadena="Tratando de aprender el uso de gibhub";
+	echo $cadena;
+?>
+<br>
+
